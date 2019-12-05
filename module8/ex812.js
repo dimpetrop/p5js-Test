69312842214f1e5887f93833cee5623d6082581c
Dimitris Petropoulos <petrojim2000@gmail.com>
2019-12-05 19:59:55 +0200

    MADE THE FACE SMEXIER

--- module8/ex812.js
+++ module8/ex812.js
@@ -1,23 +1,47 @@
-let	x= 300;
-let	y= 200;
-let	speedx= 4;
-let speedy= -3;
-let r = 24;
 function setup() {
-	createCanvas(600,400);
-}	
-function draw() {
-	background(0);
-	strokeWeight(4);
-	stroke(255);
-	fill(255, 0, 0);
-	x = x + speedx;
-	y = y + speedy;
-	if (x > (width-r/2) || x- r/2< 0) {
-		speedx = speedx* (-1);
-	}
-	if (y> (height-r/2)  || y -r/2 < 0) {
-		speedy = speedy* (-1);
-	}
-	ellipse(x, y ,r);	
-}
+	createCanvas(800, 600);
+	// set background color
+	background('#D66761');
+  }
+  function draw() {
+	// state the fill color of the shape
+	fill('#FBEFCC');
+	// state the border color of the shape
+	stroke('#FBEFCC');
+	// state the border weight of the shape
+	strokeWeight(2);
+	// create the face ellipse
+	ellipse(width/2,height/2,200,300)
+	// state the border color of the outer eye circle
+	stroke('#A2836E');
+	// for the left eye
+	// draw the base ellipse, the sclera/“white of the eye”
+	ellipse(350,275,50,25);
+	// state the border of the iris
+	stroke('black');
+	// draw the Iris for the eye
+	ellipse(350,275,25);
+	// fill the center of the eye
+	fill('#034F84');
+	// color the border
+	stroke('#034F84');
+	// draw the center of the eye
+	ellipse(350,275,12.5);
+	// state the border color of the outer eye circle
+	stroke('#A2836E');
+	// for the right eye
+	// draw the base ellipse for the eye, the sclera/“white of the eye”
+	noFill();
+	ellipse(450,275,50,25);
+	// state the border of the iris
+	stroke('black');
+	// draw the Iris for the eye
+	ellipse(450,275,25);
+	// fill the center of the eye
+	fill('#034F84');
+	// color the border
+	stroke('#034F84');
+	// draw the center of the eye
+	ellipse(450,275,12.5);
+  }
+  
