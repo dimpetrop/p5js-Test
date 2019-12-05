0fc7f67cd496c120f6692349b464d56818fd2669
Dimitris Petropoulos <petrojim2000@gmail.com>
2019-12-05 19:09:41 +0200

    I am 3D now!

--- module8/ex812.js
+++ module8/ex812.js
@@ -1,15 +1,16 @@
-let xPos;
 function setup() {
-  // create canvas
-  createCanvas(800,500);
-  // set background color
-  background('orange');
-}
-function draw () {
-  // use a for loop and create points with a distance of 30px
-  for (xPos = 1; xPos<width; xPos = xPos +30) {
-    // in order to create a diagonal line of points going from up left to down right
-    // you will have to use the appropiate geometry to define the y coordinate
-    point (xPos,5/8*xPos);
+	// create canvas
+	createCanvas(800,500);
   }
-}
+  function draw () {
+	// set background color
+	background('orange');
+	noFill();
+	// create the cube
+	rect(300,200,100,100);
+	rect(350,250,100,100);
+	line (400,200,450,250);
+	line (300,200,350,250);
+	line (400,300,450,350);
+	line (300,300,350,350);
+  }
